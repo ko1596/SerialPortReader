b6f996c4fd41dc8364c2e281d0a99ba184a63904
ko1596 <ko89022189@gmail.com>
2021-07-06 10:07:32 +0800

    2ed rs232 transport in windows

--- serilport.c
+++ serilport.c
@@ -1,26 +1,73 @@
-
-#include<stdio.h>
-#include<windows.h>
+#include <Windows.h>
+#include <stdio.h>
  
-int main()
+HANDLE hCom;
+ 
+int main(void)
 {
-	FILE *fp;
-	if ((fp = fopen("com5", "r")) == NULL)
+	hCom = CreateFile(TEXT("com5"),//COM1口
+		GENERIC_READ, //允许读
+		0, //指定共享属性，由于串口不能共享，所以该参数必须为0
+		NULL,
+		OPEN_EXISTING, //打开而不是创建
+ 
+		FILE_ATTRIBUTE_NORMAL, //属性描述，该值为FILE_FLAG_OVERLAPPED，表示使用异步I/O，该参数为0，表示同步I/O操作
+		NULL);
+ 
+	if (hCom == INVALID_HANDLE_VALUE)
 	{
-		printf("cannot open com!\n");
+		printf("open COM unsucessus!\n");
+		return FALSE;
 	}
 	else
-		printf("open com successful!\n");
+	{
+		printf("open COM sucessused!\n");
+	}
  
-	char str;
+	SetupComm(hCom, 1024, 1024); //输入缓冲区和输出缓冲区的大小都是1024
+ 
+	/*********************************超时设置**************************************/
+	COMMTIMEOUTS TimeOuts;
+	//设定读超时
+	TimeOuts.ReadIntervalTimeout = MAXDWORD;//读间隔超时
+	TimeOuts.ReadTotalTimeoutMultiplier = 0;//读时间系数
+	TimeOuts.ReadTotalTimeoutConstant = 0;//读时间常量
+	//设定写超时
+	TimeOuts.WriteTotalTimeoutMultiplier = 1;//写时间系数
+	TimeOuts.WriteTotalTimeoutConstant = 1;//写时间常量
+	SetCommTimeouts(hCom, &TimeOuts); //设置超时
+ 
+	/*****************************************配置串口***************************/
+	DCB dcb;
+	GetCommState(hCom, &dcb);
+	dcb.BaudRate = 115200; //波特率为9600
+	dcb.ByteSize = 8; //每个字节有8位
+	dcb.Parity = NOPARITY; //无奇偶校验位
+	dcb.StopBits = ONESTOPBIT; //一个停止位
+	SetCommState(hCom, &dcb);
+ 
+	DWORD wCount;//实际读取的字节数
+	boolean bReadStat;
+ 
+	char str[2] = { 0 };
  
 	while (1)
 	{
-		fscanf(fp, "%c", &str);
-		printf("%c ", str);
+		//PurgeComm(hCom, PURGE_TXCLEAR | PURGE_RXCLEAR); //清空缓冲区
+		bReadStat = ReadFile(hCom, str, sizeof(str), &wCount, NULL);
  
-		Sleep(100);
+		if (!bReadStat)
+		{
+			printf("read COM fail!");
+			return FALSE;
+		}
+		else
+		{
+			str[1] = '\0';
+			printf("%c", str[0]);
+		}
+		Sleep(10);
 	}
  
-	return 0;
+	CloseHandle(hCom);
 }
